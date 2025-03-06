ef3408cd1a9b3e44d53f646c9b2b651dec01d00c
zzznise <keirscr@gmail.com>
2025-03-07 01:17:44 +0800

    Add files via upload

---
+++ script111.js
@@ -0,0 +1,10 @@
+// Sidebar Menu Toggle (if needed in the future)
+document.querySelector(".menu-btn").addEventListener("click", function() {
+    alert("Sidebar menu clicked! (Feature coming soon)");
+});
+
+
+// Sidebar Menu Toggle (if needed in the future)
+document.querySelector(".menu-btn").addEventListener("click", function() {
+    alert("Sidebar menu clicked! (Feature coming soon)");
+});
