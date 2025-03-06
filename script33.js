932c32ca9e4d1525b2a55a73dca02377f04c44ef
zzznise <keirscr@gmail.com>
2025-03-07 01:04:45 +0800

    Add files via upload

---
+++ script33.js
@@ -0,0 +1,14 @@
+// Back button action
+document.querySelector(".back-btn").addEventListener("click", function() {
+    alert("Go back clicked! (Feature coming soon)");
+});
+
+// Close button action
+document.querySelector(".close-btn").addEventListener("click", function() {
+    alert("Close profile clicked! (Feature coming soon)");
+});
+
+// Edit button action
+document.querySelector(".edit-btn").addEventListener("click", function() {
+    alert("Edit profile clicked! (Feature coming soon)");
+});
